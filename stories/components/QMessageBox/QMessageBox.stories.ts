b6cbc8b7a3ee772d83faf13d7d35b4c4e97c4e41
v.zheleztsov <v.zheleztsov@qvant.ru>
2021-03-29 17:38:32 +0300

    fix by comments

--- stories/components/QMessageBox/QMessageBox.stories.ts
+++ stories/components/QMessageBox/QMessageBox.stories.ts
@@ -5,7 +5,7 @@ export default {
   title: 'Components/QMessageBox',
   component: QMessageBox,
   argTypes: {
-    modelValue: { control: { type: 'none' } }
+    isVisible: { control: { type: 'none' } }
   }
 };
 

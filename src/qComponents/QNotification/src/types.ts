afcf01a442af64fd96d09d4759aafd3190c7bc4a
ViZhe <v.zheleztsov@qvant.ru>
2021-04-26 17:47:14 +0300

    [QNotification] Migrate to TS + Vue3 + Composition API (#28)

---
+++ src/qComponents/QNotification/src/types.ts
@@ -0,0 +1,51 @@
+import { Ref, ComputedRef } from 'vue';
+
+export type QNotificationCloudPropType =
+  | 'success'
+  | 'warning'
+  | 'info'
+  | 'error';
+
+export type QNotificationCloudPropOnClose = Nullable<(id: string) => void>;
+
+export interface QNotificationProps {
+  icon: Nullable<string>;
+  duration: Nullable<number>;
+}
+
+export interface QNotificationCloudItem {
+  id?: string;
+  message?: Nullable<string>;
+  type?: QNotificationCloudPropType;
+  dangerouslyUseHTMLString?: Nullable<boolean>;
+  icon?: Nullable<string>;
+  duration?: Nullable<number>;
+  onClose?: QNotificationCloudPropOnClose;
+}
+
+export type QNotificationMethodAddCloud = (
+  cloud?: QNotificationCloudItem
+) => void;
+
+export interface QNotificationInstance {
+  clouds: Ref<QNotificationCloudItem[]>;
+  removeCloud: (cloudId?: string | null) => void;
+}
+
+export interface QNotificationCloudProps {
+  uniqId: string;
+  message: Nullable<string>;
+  type: QNotificationCloudPropType;
+  dangerouslyUseHtmlString: Nullable<boolean>;
+  icon: Nullable<string>;
+  duration: Nullable<number>;
+  onClose: QNotificationCloudPropOnClose;
+}
+
+export interface QNotificationCloudInstance {
+  notificationClasses: ComputedRef<Record<string, boolean>>;
+  iconClass: ComputedRef<string>;
+  clearTimer: () => void;
+  startTimer: () => void;
+  close: () => void;
+}
---
+++ src/qComponents/helpers/eventBus.ts
@@ -0,0 +1,6 @@
+import mitt from 'mitt';
+
+const eventBus = mitt();
+
+// eslint-disable-next-line import/prefer-default-export
+export { eventBus };

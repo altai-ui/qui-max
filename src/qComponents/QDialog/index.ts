68e781ac4b49fabd1fca1b1b3672d8292885b6f8
ViZhe <v.zheleztsov@qvant.ru>
2021-05-11 10:54:03 +0300

    Add QDialog (#29)

---
+++ src/qComponents/QDialog/index.ts
@@ -0,0 +1,11 @@
+import { App } from 'vue';
+
+import QDialog from './src/QDialog.vue';
+
+/* istanbul ignore next */
+QDialog.install = (app: App): void => {
+  app.component(QDialog.name, QDialog);
+};
+
+export type { QDialogProps, QDialogInstance } from './src/types';
+export default QDialog;

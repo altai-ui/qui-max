ba5a2b6e1184e99e9afaf5791b135c1a8e06dd90
v.zheleztsov <v.zheleztsov@qvant.ru>
2021-06-18 15:58:33 +0300

    fix import

--- src/qComponents/QTable/src/QTableTHead/QTableTHead.vue
+++ src/qComponents/QTable/src/QTableTHead/QTableTHead.vue
@@ -28,13 +28,13 @@ import { defineComponent, ref, computed, inject } from 'vue';
 import { isEmpty, cloneDeep } from 'lodash-es';
 
 import { TOTAL_CHECKED_INDEX } from '../config';
-import QTableTHeadCell from './QTableTHeadCell.vue';
 import QTableCellCheckbox from '../QTableCellCheckbox/QTableCellCheckbox.vue';
 import type { QTablePropSortBy, QTableProvider } from '../types';
 import type {
   ExtendedColumn,
   QTableContainerProvider
 } from '../QTableContainer/types';
+import QTableTHeadCell from './QTableTHeadCell/QTableTHeadCell.vue';
 import type { QTableTHeadInstance } from './types';
 
 export default defineComponent({

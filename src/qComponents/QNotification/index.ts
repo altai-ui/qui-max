1a3e5f081f061344b56670a1377649337e823887
v.zheleztsov <v.zheleztsov@qvant.ru>
2021-08-05 13:15:20 +0300

    feat: rename and export notify injection key

--- src/qComponents/QNotification/index.ts
+++ src/qComponents/QNotification/index.ts
@@ -2,30 +2,30 @@ import { getCurrentInstance, provide, inject } from 'vue';
 import type { App, Plugin } from 'vue';
 
 import { createNotification } from './src';
-import { NotifyType, NOTIFICATION_INJECTION_KEY } from './src/constants';
+import { NotifyType, Q_NOTIFY_INJECTION_KEY } from './src/constants';
 import type { QNotify, QNotificationOptions } from './src/types';
 
 const QNotification: Plugin = {
   install: (app: App, options?: QNotificationOptions): void => {
     const notify = createNotification(options);
-    app.provide<QNotify>(NOTIFICATION_INJECTION_KEY, notify);
+    app.provide<QNotify>(Q_NOTIFY_INJECTION_KEY, notify);
   }
 };
 
 const provideNotify = (options?: QNotificationOptions): void => {
   const notify = createNotification(options);
 
-  provide<QNotify>(NOTIFICATION_INJECTION_KEY, notify);
+  provide<QNotify>(Q_NOTIFY_INJECTION_KEY, notify);
 };
 
 const useNotify = (options?: QNotificationOptions): QNotify => {
   const notify = getCurrentInstance()
-    ? inject<QNotify>(NOTIFICATION_INJECTION_KEY)
+    ? inject<QNotify>(Q_NOTIFY_INJECTION_KEY)
     : null;
 
   return notify ?? createNotification(options, false);
 };
 
 export type { QNotify, QNotifyId } from './src/types';
-export { NotifyType, provideNotify, useNotify };
+export { Q_NOTIFY_INJECTION_KEY, NotifyType, provideNotify, useNotify };
 export default QNotification;

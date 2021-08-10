f05429243fbe7f2d0a7ccebacef101d85a934572
Cheesytim <darknelmail.com@mail.ru>
2021-08-10 16:14:24 +0300

    [QTextarea] Minor fixes (#144)

--- src/qComponents/QTextarea/src/calcTextareaHeight.ts
+++ src/qComponents/QTextarea/src/calcTextareaHeight.ts
@@ -43,16 +43,23 @@ function calculateNodeStyling(targetElement: HTMLTextAreaElement): NodeStyling {
   const boxSizing = style.getPropertyValue('box-sizing');
 
   const paddingSize =
-    parseFloat(style.getPropertyValue('padding-bottom')) +
-    parseFloat(style.getPropertyValue('padding-top'));
+    (parseFloat(style.getPropertyValue('padding-bottom')) || 0) +
+    (parseFloat(style.getPropertyValue('padding-top')) || 0);
 
   const borderSize =
-    parseFloat(style.getPropertyValue('border-bottom-width')) +
-    parseFloat(style.getPropertyValue('border-top-width'));
+    (parseFloat(style.getPropertyValue('border-bottom-width')) || 0) +
+    (parseFloat(style.getPropertyValue('border-top-width')) || 0);
 
-  const contextStyle = CONTEXT_STYLE.map(
-    name => `${name}:${style.getPropertyValue(name)}`
-  ).join(';');
+  const contextStyles: string[] = [];
+
+  CONTEXT_STYLE.forEach(styleName => {
+    const styleValue = style.getPropertyValue(styleName);
+    if (styleValue !== '') {
+      contextStyles.push(`${styleName}:${styleValue}`);
+    }
+  });
+
+  const contextStyle: string = contextStyles.join(';');
 
   return { contextStyle, paddingSize, borderSize, boxSizing };
 }

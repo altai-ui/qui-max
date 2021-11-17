024bb390180cacf2acb6fd21480012dc52de3c28
Tim Bochkarev <darknelmail.com@mail.ru>
2021-11-17 15:06:06 +0300

    fix: change base value in config

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,6 +3,8 @@ import { defineUserConfig } from '@vuepress/cli'
 import type { DefaultThemeOptions } from '@vuepress/theme-default'
 
 export default defineUserConfig<DefaultThemeOptions, ViteBundlerOptions>({
+  // The base URL the site will be deployed at:
+  base: '/qui-max/',
   lang: 'en-US',
   title: 'Qui Max',
   description: 'Neumorphic design system for Web',

f96ef5ecbf1909f04d6ca124aa7c48e0cd4af28c
v.zheleztsov <v.zheleztsov@qvant.ru>
2022-02-10 14:04:54 +0300

    fix(QTable): do not remount all rows on one row's change

--- src/qComponents/QTable/src/QTableTBody/types.ts
+++ src/qComponents/QTable/src/QTableTBody/types.ts
@@ -3,4 +3,5 @@ import type { ComputedRef } from 'vue';
 export interface QTableTBodyInstance {
   randId: (prefix: string) => string;
   rowsList: ComputedRef<Record<string, unknown>[]>;
+  getHash: (obj: Record<string, unknown>) => string;
 }

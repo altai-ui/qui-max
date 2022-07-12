b7a97e6b03864d1042d1b23f020dcba3e1623c7f
Vasiliy Rusin <RusinVA@gmail.com>
2022-07-12 16:23:43 +0700

    feat(QCheckbox): add labelSize props and redesign (#306)

--- src/qComponents/QCheckbox/index.ts
+++ src/qComponents/QCheckbox/index.ts
@@ -4,4 +4,14 @@ import Checkbox from './src/QCheckbox.vue';
 
 export const QCheckbox = withInstall(Checkbox);
 
-export type { QCheckboxProps, QCheckboxInstance } from './src/types';
+export type {
+  QCheckboxProps,
+  QCheckboxInstance,
+  QCheckboxPropModelValue,
+  QCheckboxPropLabel,
+  QCheckboxPropIndeterminate,
+  QCheckboxPropDisabled,
+  QCheckboxPropRootTag,
+  QCheckboxPropValidateEvent,
+  QCheckboxPropLabelSize
+} from './src/types';
--- src/qComponents/QCheckbox/src/types.ts
+++ src/qComponents/QCheckbox/src/types.ts
@@ -1,22 +1,36 @@
 import type { Ref, ComputedRef } from 'vue';
 
-import type { Nullable } from '#/helpers';
+import type { Nullable, ClassValue } from '#/helpers';
+
+export type QCheckboxPropModelValue = Nullable<boolean>;
+export type QCheckboxPropLabel = Nullable<string>;
+export type QCheckboxPropIndeterminate = Nullable<boolean>;
+export type QCheckboxPropDisabled = Nullable<boolean>;
+export type QCheckboxPropRootTag = Nullable<string>;
+export type QCheckboxPropValidateEvent = Nullable<boolean>;
+export type QCheckboxPropLabelSize = Nullable<'regular' | 'small'>;
 
 export interface QCheckboxProps {
-  modelValue: Nullable<boolean>;
-  label: Nullable<string>;
-  indeterminate: Nullable<boolean>;
-  disabled: Nullable<boolean>;
-  rootTag: Nullable<string>;
-  validateEvent: Nullable<boolean>;
+  modelValue: QCheckboxPropModelValue;
+  label: QCheckboxPropLabel;
+  indeterminate: QCheckboxPropIndeterminate;
+  disabled: QCheckboxPropDisabled;
+  rootTag: QCheckboxPropRootTag;
+  validateEvent: QCheckboxPropValidateEvent;
+  labelSize: QCheckboxPropLabelSize;
 }
 
 export interface QCheckboxInstance {
   focus: Ref<boolean>;
   isChecked: ComputedRef<boolean>;
+  isIndeterminate: ComputedRef<boolean>;
   isLimitDisabled: ComputedRef<boolean>;
   isDisabled: ComputedRef<boolean>;
   nativeClick: () => void;
   handleCheckboxClick: (event: Event) => void;
   checkboxInput: Ref<Nullable<HTMLInputElement>>;
+  labelClass: ComputedRef<ClassValue>;
+  qCheckboxClasses: ComputedRef<ClassValue>;
+  qCheckboxInputClasses: ComputedRef<ClassValue>;
+  qCheckboxInnerIconClasses: ComputedRef<ClassValue>;
 }
